5ac8bd000fbf5a2c411b718c8e09288639a16b84
tyronejayy <nuhaadf@gmail.com>
2018-11-07 11:58:53 +0200

    First commit

---
+++ alert.js
@@ -0,0 +1,6 @@
+// Take the solution of the previous task Show and alert. Modify it by extracting the script content into an external file alert.js, residing in the same folder.
+
+// alert("Im loving it!");
+
+var name = prompt("Enter your name:");
+alert("Hello " + name)
---
+++ main.js
@@ -0,0 +1,57 @@
+// /'use strict'; // the "use strict" directive switches the engine to the "modern" mode, changing the behaviour of some built-in features
+
+// variables are used to store this information.
+
+// A variable is a "named storage" for data. We can use variables to store data.
+
+// To create a variable in Javascript, we need to use the let or var keyword.
+
+var message
+
+// assignment operator =
+
+// We add a string ''
+
+// A string in Javascript is a set characters surrounded by quotation marks.
+
+var message = 'Good Morning';
+    // message = 'How are you today?';
+alert(message);
+
+// var name = 'Nuhaa';
+        // ages = 21;
+//     gender = 'female';
+// alert(age);
+
+
+// *******************************************************************
+// *******************************************************************
+
+// Always remember to close a string with the same type of quotation marks you started it with.
+
+// Operators:
+
+// + addition
+// - subtraction
+// / division
+// * multiplication
+
+// the + operator can be applied to two strings values. This will join two strings together, otherwise called Concatenation.
+
+// eg. In the console "Hey" + " there."
+
+// Choose meaningful names
+// Don't use reserved words
+// Follow conventions
+
+/* https:
+//developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+/Lexical_grammar#Keywords */
+
+// What's a condition?
+
+var name = "Nuhaa",
+  surname = "Koert",
+          age = 21;
+console.log("I am " + name +" " +  surname + " and I am " + age  + " years old")
+
